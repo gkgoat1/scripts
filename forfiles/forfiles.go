493ae29345d2dae2e2d17065df736eb98408413a
Graham Kelly <gkgoat6700@gmail.com>
2025-12-23 16:35:29 -0500

    add forfiles

---
+++ forfiles/forfiles.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	// Create a new scanner that reads from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	replace := os.Args[1]
+	cmd := os.Args[2:]
+
+	// The Scan method blocks until a line is available, an error occurs, or EOF is reached.
+	for scanner.Scan() {
+		line := scanner.Text() // Get the current line of text
+
+		// Process the line (e.g., print it, modify it, etc.)
+		cmd2 := make([]string, len(cmd))
+		for i, s := range cmd {
+			cmd2[i] = strings.ReplaceAll(s, replace, line)
+		}
+		go func(cmd2 []string) {
+			out, err := exec.Command(cmd2[0], cmd2[1:]...).CombinedOutput()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "command execution error:", err)
+			}
+			fmt.Print(string(out))
+		}(cmd2)
+	}
+
+	// Check for errors after the loop
+	if err := scanner.Err(); err != nil {
+		// Note: io.EOF is not an error in this context and is handled by the loop termination.
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gkgoat1/scripts
+
+go 1.25.5
