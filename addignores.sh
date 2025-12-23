a7b0c82787cf79e836cc9fedc7fe48b4546e5dfe
Graham Kelly <gkgoat6700@gmail.com>
2025-12-23 16:10:43 -0500

    fix

--- addignores.sh
+++ addignores.sh
@@ -1,2 +1,2 @@
-ls | xargs -I '^' sh -c 'cd ^; echo target >> .gitignore; echo node_modules >> .gitignore; echo .DS_Store >> .gitignore'
+ls | xargs -I '^' sh -c 'cd ^;echo >> .gitignore; echo target >> .gitignore; echo node_modules >> .gitignore; echo .DS_Store >> .gitignore'
 sh $(dirname $0)/sortallignores.sh
