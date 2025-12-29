779ddd33ea4fd179482036dc6b771030426b0b57
Graham Kelly <gkgoat6700@gmail.com>
2025-12-29 09:05:59 -0500

    use gh cli in variant

---
+++ git/fetch-repos-gh.sh
@@ -0,0 +1,1 @@
+gh repo list $1 --source -L 400 --json name | python3 -c "import sys,json;data=json.load(sys.stdin);print('\n'.join(f'{r[\"name\"]}' for r in data))" | xargs -n 1 sh $(dirname $0)/pull.sh $1
