140fb0d5efa4dfabbec2de25c80b8a35c35e2ec0
Graham Kelly <gkgoat6700@gmail.com>
2026-01-04 22:39:18 -0500

    update to use forfiles

--- git/fetch-repos-gh.sh
+++ git/fetch-repos-gh.sh
@@ -1,1 +1,1 @@
-gh repo list $1 --source -L 400 --json name | python3 -c "import sys,json;data=json.load(sys.stdin);print('\n'.join(f'{r[\"name\"]}' for r in data))" | xargs -n 1 sh $(dirname $0)/pull.sh $1
+gh repo list $1 --source -L 400 --json name | python3 -c "import sys,json;data=json.load(sys.stdin);print('\n'.join(f'{r[\"name\"]}' for r in data))" | go run $(dirname $0)/../forfiles/forfiles.go '^' sh $(dirname $0)/pull.sh $1 '^'
